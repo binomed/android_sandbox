804852290ae06cbccb7f2433fa38bc4a0e8ab37f
jefBinomed <jean.francois.garreau@gmail.com>
2011-07-26 13:56:12 +0200

    ajout d'un projet sandbox utilisant andEngine

---
+++ AndEngineSandBox/src/com/binomed/andengine/SplashExample.java
@@ -0,0 +1,44 @@
+package com.binomed.andengine;
+
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.source.AssetBitmapTextureAtlasSource;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.anddev.andengine.ui.activity.BaseSplashActivity;
+
+import android.app.Activity;
+
+public class SplashExample extends BaseSplashActivity {
+
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final int SPLASH_DURATION = 3;
+	private static final float SPLASH_SCALE_FROM = 1f;
+
+	@Override
+	protected ScreenOrientation getScreenOrientation() {
+		return ScreenOrientation.LANDSCAPE;
+	}
+
+	@Override
+	protected IBitmapTextureAtlasSource onGetSplashTextureAtlasSource() {
+		return new AssetBitmapTextureAtlasSource(this, "gfx/splash.png");
+	}
+
+	@Override
+	protected float getSplashDuration() {
+		return SPLASH_DURATION;
+	}
+
+	@Override
+	protected Class<? extends Activity> getFollowUpActivity() {
+		return AndEngineSandBoxActivity.class;
+	}
+
+	@Override
+	protected float getSplashScaleFrom() {
+		return SPLASH_SCALE_FROM;
+	}
+
+}
