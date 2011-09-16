c9174abaf75ddd98ac48e0d3a07923dc22a8ab62
jefBinomed <jean.francois.garreau@gmail.com>
2011-09-16 14:00:57 +0200

    mise en place suppression du peg

--- AndEngineSandBox/src/com/binomed/andengine/SplashExample.java
+++ AndEngineSandBox/src/com/binomed/andengine/SplashExample.java
@@ -1,17 +1,10 @@
 package com.binomed.andengine;
 
-import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
-import org.anddev.andengine.opengl.texture.atlas.bitmap.source.AssetBitmapTextureAtlasSource;
-import org.anddev.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
-import org.anddev.andengine.ui.activity.BaseSplashActivity;
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class SplashExample extends Activity {
 
@@ -20,38 +13,47 @@ public class SplashExample extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		
 		Button btnTank = (Button) findViewById(R.id.tank);
 		Button btnPeg = (Button) findViewById(R.id.peg);
+		Button btnPegBis = (Button) findViewById(R.id.pegbis);
 		Button btnTest = (Button) findViewById(R.id.tests);
-		
+
 		btnTank.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(getBaseContext(), MonActivite.class);
-				startActivity(intent );
+				startActivity(intent);
 			}
 		});
 		btnPeg.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(getBaseContext(), MonActiviteInteraction.class);
-				startActivity(intent );
-				
+				startActivity(intent);
+
+			}
+		});
+		btnPegBis.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getBaseContext(), MonActiviteDansUneVue.class);
+				startActivity(intent);
+
 			}
 		});
 		btnTest.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(getBaseContext(), MonJeuActivite.class);
-				startActivity(intent );
-				
+				startActivity(intent);
+
 			}
 		});
 
 	}
-	
-	}
+
+}
