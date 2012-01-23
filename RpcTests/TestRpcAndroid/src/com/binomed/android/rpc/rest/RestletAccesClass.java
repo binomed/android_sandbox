285c464d83c546d0984177099f75c6229795b806
jefBinomed <jean.francois.garreau@gmail.com>
2012-01-23 18:15:16 +0100

    validation méthode RestLet

--- RpcTests/TestRpcAndroid/src/com/binomed/android/rpc/rest/RestletAccesClass.java
+++ RpcTests/TestRpcAndroid/src/com/binomed/android/rpc/rest/RestletAccesClass.java
@@ -1,5 +1,8 @@
 package com.binomed.android.rpc.rest;
 
+import org.restlet.engine.Engine;
+import org.restlet.ext.httpclient.HttpClientHelper;
+import org.restlet.ext.jackson.JacksonConverter;
 import org.restlet.resource.ClientResource;
 
 import com.binomed.android.rpc.TestRpcAndroidActivity;
@@ -9,6 +12,11 @@ import com.binomed.client.rest.dto.RestletObjectA;
 public class RestletAccesClass {
 
 	public static RestletObjectA callService() throws Exception {
+
+		Engine.getInstance().getRegisteredConverters().add(new JacksonConverter());
+		Engine.getInstance().getRegisteredClients().clear();
+		Engine.getInstance().getRegisteredClients().add(new HttpClientHelper(null));
+
 		ClientResource clientResource = new ClientResource(TestRpcAndroidActivity.LOCALHOST + "/rest/");
 
 		IRestletService service = clientResource.wrap(IRestletService.class);
