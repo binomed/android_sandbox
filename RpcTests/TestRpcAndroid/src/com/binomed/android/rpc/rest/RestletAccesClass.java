7a511204aa0a4bbda0b32f7578ef8fd0db9d9ccc
jefBinomed <jean.francois.garreau@gmail.com>
2012-01-25 18:14:50 +0100

    refactoring

--- RpcTests/TestRpcAndroid/src/com/binomed/android/rpc/rest/RestletAccesClass.java
+++ RpcTests/TestRpcAndroid/src/com/binomed/android/rpc/rest/RestletAccesClass.java
@@ -14,15 +14,26 @@ import com.binomed.client.rest.dto.RestletObjectB;
 
 public class RestletAccesClass {
 
+	private static ClientResource resource;
+	private static ClientResource resourceWithParam;
+	private static IRestletService service;
+	private static IRestletService serviceWithParam;
+
+	private static synchronized void init() {
+		if (resource == null) {
+			Engine.getInstance().getRegisteredConverters().add(new JacksonConverter());
+			Engine.getInstance().getRegisteredClients().clear();
+			Engine.getInstance().getRegisteredClients().add(new HttpClientHelper(null));
+			resource = new ClientResource(TestRpcAndroidActivity.LOCALHOST + "/rest/test");
+			resourceWithParam = new ClientResource(TestRpcAndroidActivity.LOCALHOST + "/rest/testParam");
+			service = resource.wrap(IRestletService.class);
+			serviceWithParam = resourceWithParam.wrap(IRestletService.class);
+		}
+	}
+
 	public static RestletObjectA callService() throws Exception {
 
-		Engine.getInstance().getRegisteredConverters().add(new JacksonConverter());
-		Engine.getInstance().getRegisteredClients().clear();
-		Engine.getInstance().getRegisteredClients().add(new HttpClientHelper(null));
-
-		ClientResource clientResource = new ClientResource(TestRpcAndroidActivity.LOCALHOST + "/rest/test");
-
-		IRestletService service = clientResource.wrap(IRestletService.class);
+		init();
 
 		RestletObjectA result = service.getMessage();
 
@@ -30,21 +41,14 @@ public class RestletAccesClass {
 	}
 
 	public static RestletObjectA callServiceWithParam() throws Exception {
-
-		Engine.getInstance().getRegisteredConverters().add(new JacksonConverter());
-		Engine.getInstance().getRegisteredClients().clear();
-		Engine.getInstance().getRegisteredClients().add(new HttpClientHelper(null));
-
-		ClientResource clientResource = new ClientResource(TestRpcAndroidActivity.LOCALHOST + "/rest/testParam");
-
-		IRestletService service = clientResource.wrap(IRestletService.class);
+		init();
 
 		RestletObjectB objB = new RestletObjectB();
 		objB.setName("Name with Rest");
 		objB.setMap(new HashMap<String, String>());
 		objB.getMap().put("key", "value");
 
-		RestletObjectA result = service.getMessageWithParameter(objB);
+		RestletObjectA result = serviceWithParam.getMessageWithParameter(objB);
 
 		return result;
 	}
