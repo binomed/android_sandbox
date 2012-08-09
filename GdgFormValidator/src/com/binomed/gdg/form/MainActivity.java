a3559c5cffe32770435795d7221559bc1ba4400d
jefBinomed <jean.francois.garreau@gmail.com>
2012-08-09 23:53:54 +0200

    Validation connexion a gDrive

--- GdgFormValidator/src/com/binomed/gdg/form/MainActivity.java
+++ GdgFormValidator/src/com/binomed/gdg/form/MainActivity.java
@@ -1,6 +1,5 @@
 package com.binomed.gdg.form;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import android.accounts.Account;
@@ -13,18 +12,18 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.MediaStore.Files;
 import android.util.Log;
 import android.view.View;
 
 import com.binomed.gdg.form.tools.IntentIntegrator;
 import com.binomed.gdg.form.tools.IntentResult;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.auth.oauth2.draft10.AccessProtectedResource;
 import com.google.api.client.extensions.android2.AndroidHttp;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.draft10.GoogleAccessProtectedResource;
+import com.google.api.client.googleapis.GoogleHeaders;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson.JacksonFactory;
@@ -36,16 +35,22 @@ import com.google.api.services.drive.model.FileList;
 public class MainActivity extends Activity {
 
 	private static final int DIALOG_ACCOUNTS = 1;
-	private static final String AUTH_TOKEN_TYPE = "oauth2:"+DriveScopes.DRIVE_FILE;
-	
+	private static final int DIALOG_FORM = 2;
+	private static final String AUTH_TOKEN_TYPE = "oauth2:" + DriveScopes.DRIVE_FILE + " " + DriveScopes.DRIVE + " " + DriveScopes.DRIVE_READONLY + " " + DriveScopes.DRIVE_METADATA_READONLY;
+	private static final String APP_NAME = "GdgFormValidator";
+	private FileList list;
+
 	private static final String TAG = "GdgFormValidator";
-	
+
 	private AccountManager accountManager;
+
+	private MainActivity activity;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		activity = this;
 
 		accountManager = AccountManager.get(this);
 		Account[] accounts = accountManager.getAccountsByType("com.google");
@@ -55,16 +60,14 @@ public class MainActivity extends Activity {
 			gotAccount(accounts[0]);
 		}
 	}
-	
 
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		switch (id) {
-		case DIALOG_ACCOUNTS:
+		case DIALOG_ACCOUNTS: {
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setTitle("Select a Google account");
-			final Account[] accounts = accountManager
-					.getAccountsByType("com.google");
+			final Account[] accounts = accountManager.getAccountsByType("com.google");
 			final int size = accounts.length;
 			String[] names = new String[size];
 			for (int i = 0; i < size; i++) {
@@ -79,87 +82,139 @@ public class MainActivity extends Activity {
 			});
 			return builder.create();
 		}
+		case DIALOG_FORM: {
+			AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+			builder.setTitle("Select a Google Form");
+			final int size = list.getItems().size();
+			String[] names = new String[size];
+			for (int i = 0; i < size; i++) {
+				names[i] = list.getItems().get(i).getTitle();
+			}
+			builder.setItems(names, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					// Stuff to do when the account is selected by the user
+					gotForm(list.getItems().get(which).getId());
+				}
+
+			});
+			return builder.create();
+		}
+		}
 		return null;
 	}
 
-	private void gotAccount(Account account) {
-		accountManager.getAuthToken(account, AUTH_TOKEN_TYPE, null, this,
-				new AccountManagerCallback<Bundle>() {
-					public void run(AccountManagerFuture<Bundle> future) {
-						try {
-							// If the user has authorized your application to
-							// use the tasks API
-							// a token is available.
-							String token = future.getResult().getString(
-									AccountManager.KEY_AUTHTOKEN);
-							// Now you can use the Tasks API...
-							useDriveAPI(token);
-						} catch (OperationCanceledException e) {
-							// TODO: The user has denied you access to the API,
-							// you should handle that
-						} catch (Exception e) {
-							Log.e(TAG, e.getLocalizedMessage(),
-									e);
-						}
-					}
-				}, null);
+	private void gotAccount(final Account account) {
+		accountManager.getAuthToken(account, AUTH_TOKEN_TYPE, null, this, new AccountManagerCallback<Bundle>() {
+			public void run(AccountManagerFuture<Bundle> future) {
+				try {
+					// If the user has authorized your application to
+					// use the tasks API
+					// a token is available.
+					String token = future.getResult().getString(AccountManager.KEY_AUTHTOKEN);
+					// Now you can use the Tasks API...
+					useDriveAPI(token, account.name);
+				} catch (OperationCanceledException e) {
+					// TODO: The user has denied you access to the API,
+					// you should handle that
+				} catch (Exception e) {
+					Log.e(TAG, e.getLocalizedMessage(), e);
+				}
+			}
+		}, null);
 	}
 
-	protected void useDriveAPI(String accessToken) {
+	protected void useDriveAPI(final String accessToken, String accountName) {
 		// Setting up the Tasks API Service
-//		HttpTransport transport = AndroidHttp.newCompatibleTransport();
-//		AccessProtectedResource accessProtectedResource = new GoogleAccessProtectedResource(
-//				accessToken);
-		
-		
-//		// Set up the HTTP transport and JSON factory
-//		HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
-//		JsonFactory jsonFactory = new JacksonFactory();
-//		
-//		 String json = "{"+//
-//			       "\"web\": {"+//
-//			       "  \"client_id\": \"[[YOUR_CLIENT_ID]]\","+//
-//			       "  \"client_secret\": \"[[YOUR_CLIENT_SECRET]]\","+//
-//			       "  \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\","+//
-//			       "  \"token_uri\": \"https://accounts.google.com/o/oauth2/token\""+//
-//			       "}"+//
-//			    "}";//
-//		
-//		
-//		GoogleClientSecrets clientSecrets =
-//		          GoogleClientSecrets.load(jsonFactory,new ByteArrayInputStream(json.getBytes()));
-//		      flow =
-//		          new GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory, clientSecrets, SCOPES)
-//		              .setAccessType("offline").setApprovalPrompt("force").build();
-//
-//		// Set up OAuth 2.0 access of protected resources
-//		// using the refresh and access tokens, automatically
-//		// refreshing the access token when it expires
-//		Credential credential = new Credential.Builder(accessMethod)
-//		    .setJsonFactory(jsonFactory)
-//		    .setTransport(httpTransport)
-//		    .setTokenServerEncodedUrl(tokenServerEncodedUrl)
-//		    .setClientAuthentication(clientAuthentication)
-//		    .setRequestInitializer(requestInitializer)
-//		    .build();
-//		
-//		
-//		Drive service = new Drive.Builder(transport, new JacksonFactory(), accessProtectedResource) //
-//				.setApplicationName("GdgFormValidator") //
-//				.build();
-//		
-//		try {
-//			com.google.api.services.drive.Drive.Files.List request = service.files().list();
-//			FileList list = request.execute();
-//			for (File file : list.getItems()){
-//				Log.i(TAG, file.getId()+" | "+file.getTitle()+" | "+file.getMimeType());
-//			}
-//		} catch (IOException e) {
-//			Log.e(TAG, e.getLocalizedMessage(),
-//					e);
-//		}
 
-		// TODO: now use the service to query the Tasks API
+		final HttpTransport transport = AndroidHttp.newCompatibleTransport();
+		final JsonFactory jsonFactory = new JacksonFactory();
+		// final HttpRequestInitializer credential = getCredentialHttpRequest(accessToken);
+		// final HttpRequestInitializer credential = getCredentialGoogleAccount(accessToken, accountName);
+		final HttpRequestInitializer credential = getCredentialGoogle(accessToken);
+
+		AsyncTask<Void, Void, FileList> listFilesTask = new AsyncTask<Void, Void, FileList>() {
+
+			@Override
+			protected void onPostExecute(final FileList list) {
+				if (list == null) {
+					return;
+				}
+				MainActivity.this.list = list;
+				showDialog(DIALOG_FORM);
+				for (File file : list.getItems()) {
+					Log.i(TAG, file.getId() + " | " + file.getTitle() + " | " + file.getMimeType());
+				}
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+				builder.setTitle("Select a Google Form");
+				final int size = list.getItems().size();
+				String[] names = new String[size];
+				for (int i = 0; i < size; i++) {
+					names[i] = list.getItems().get(i).getTitle();
+				}
+				builder.setItems(names, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						// Stuff to do when the account is selected by the user
+						gotForm(list.getItems().get(which).getId());
+					}
+
+				});
+				builder.create().show();
+
+				Log.i(TAG, "Items listed");
+			}
+
+			@Override
+			protected FileList doInBackground(Void... params) {
+				Drive service = new Drive.Builder(transport, jsonFactory, credential) //
+						// .setJsonHttpRequestInitializer(new GoogleKeyInitializer(Keys.API_KEY)) //
+						.setApplicationName(APP_NAME)//
+						.build();
+
+				try {
+					com.google.api.services.drive.Drive.Files.List request = service.files().list() //
+							.setQ("mimeType='application/vnd.google-apps.form'")//
+					;
+					final FileList list = request.execute();
+					return list;
+
+				} catch (IOException e) {
+
+					Log.e(TAG, e.getLocalizedMessage(), e);
+				}
+				return null;
+			}
+		}.execute();
+
+	}
+
+	private void gotForm(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private HttpRequestInitializer getCredentialGoogle(final String accessToken) {
+		GoogleCredential credential = new GoogleCredential();
+		credential.setAccessToken(accessToken);
+		return credential;
+	}
+
+	private HttpRequestInitializer getCredentialGoogleAccount(final String accessToken, final String accountName) {
+		GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(accessToken, getApplicationContext(), DriveScopes.DRIVE_FILE);
+		credential.setAccountName(accountName);
+		return credential;
+	}
+
+	private HttpRequestInitializer getCredentialHttpRequest(final String accessToken) {
+		final HttpRequestInitializer credential = new HttpRequestInitializer() {
+
+			@Override
+			public void initialize(HttpRequest request) throws IOException {
+				request.getHeaders().setAuthorization(GoogleHeaders.getGoogleLoginValue(accessToken));
+
+			}
+		};
+		return credential;
 	}
 
 	public void clickEvent(View v) {
@@ -175,8 +230,7 @@ public class MainActivity extends Activity {
 	}
 
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		IntentResult scanResult = IntentIntegrator.parseActivityResult(
-				requestCode, resultCode, intent);
+		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
 		if (scanResult != null) {
 			// handle scan result
 		}
--- GdgFormValidator/src/com/binomed/gdg/form/cst/Keys.java
+++
@@ -1,7 +0,0 @@
-package com.binomed.gdg.form.cst;
-
-public class Keys {
-
-	public static final String API_KEY = "AIzaSyCZCYiIpkDdgzmXVeJihy3p-lEj33m6WtQ";
-	
-}
